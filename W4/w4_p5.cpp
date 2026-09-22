79c1877815999d861c7fb85b1c4d160206dd31db
Ruman-Khan01 <pathanruman786@gmail.com>
2026-09-22 10:51:26 +0530

    Add files via upload

---
+++ W4/w4_p5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+
+	cout << "Enter the number: ";
+	cin >> n;
+	
+	cout << "Prime numbers between 1 and " << n << "are: ";
+	
+	for (int num = 2; num <= n; num++) {
+        bool isPrime = true;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << num << " ";
+        }
+    }
+    return 0;
+}
